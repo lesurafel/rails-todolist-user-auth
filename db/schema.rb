704012d22f19f325bc1f10115e879e478a2fb26c
lesurafel <lesurafel@gmail.co>
2021-03-24 16:41:44 -0700

    Create Session table (model)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_24_230750) do
+ActiveRecord::Schema.define(version: 2021_03_24_234023) do
+
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.datetime "created_at", null: false
