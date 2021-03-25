1cc21501343d1327ac246bcb976ecb4295cb471f
lesurafel <lesurafel@gmail.co>
2021-03-24 17:09:19 -0700

    Create Session Model and add it's Attribute user id and token

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_24_234023) do
+ActiveRecord::Schema.define(version: 2021_03_24_235305) do
 
   create_table "sessions", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.string "token"
+    t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
   create_table "tasks", force: :cascade do |t|
